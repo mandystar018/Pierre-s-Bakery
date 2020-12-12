2fda1c0507b8900a372e225c412bc24425d1937e
Amanda Otero <mandystar018@gmail.com>
2020-12-11 16:10:40 -0800

    Get king of pastry test purposefully fail

--- Bakery.Solution/Bakery.Tests/ModelTests/Bakery.Test.cs
+++ Bakery.Solution/Bakery.Tests/ModelTests/Bakery.Test.cs
@@ -82,7 +82,7 @@ namespace Bakery.Tests
     [TestMethod]
     public void GetKindOfPastry_ReturnKindOfPastry_String()
     {
-      string pastry = "Croissat";
+      string pastry = "Croisat";
       Pastry newPastry = new Pastry("Croissant", 5, 1);
       string result = newPastry.Kind;
       Assert.AreEqual(pastry, result);
