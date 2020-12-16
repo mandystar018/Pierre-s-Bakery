57af1f6e08b77702a5cfcd0a104c658adb5d388f
Amanda Otero <mandystar018@gmail.com>
2020-12-15 18:41:02 -0800

    Separate two clases in two different files and one test per class

--- Bakery.Solution/Bakery.Tests/ModelTests/Pastry.Test.cs
+++ Bakery.Solution/Bakery.Tests/ModelTests/Pastry.Test.cs
@@ -2,75 +2,7 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
 
-namespace Bakery.Tests
-{
-  [TestClass]
-  public class BreadTests
-  {
-    [TestMethod]
-    public void BreadConstructor_CreatesInstanceOfBread_Bread()
-    {
-      Bread newBread = new Bread("Baguette", 5, 1);
-      Assert.AreEqual(typeof(Bread), newBread.GetType());
-    }
-
-    [TestMethod]
-    public void GetKindOfBread_ReturnKindOfBread_String()
-    {
-      string bread = "Baguette";
-      Bread newBread = new Bread("Baguette", 5, 1);
-      string result = newBread.Kind;
-      Assert.AreEqual(bread, result);
-    }
-
-    [TestMethod]
-    public void GetPriceOfBread_ReturnPriceOfBread_Int()
-    {
-      int price = 5;
-      Bread newBread = new Bread("Baguette", 5, 1);
-      int result = newBread.Price;
-      Assert.AreEqual(price, result);
-    }
-
-    [TestMethod]
-    public void SetPriceOfBread_ReturnUpdatesPriceOfBread_Int()
-    {
-      Bread newBread = new Bread("Baguette", 5, 1);
-      int updatedPrice = 10;
-      newBread.Price = updatedPrice;
-      int result = newBread.Price;
-      Assert.AreEqual(updatedPrice, result);
-    }
-
-    [TestMethod]
-    public void GetQuantityOfBread_ReturnQuantityOfBread_Int()
-    {
-      int quantity = 1;
-      Bread newBread = new Bread("Baguette", 5, 1);
-      int result = newBread.Quantity;
-      Assert.AreEqual(quantity, result);
-    }
-
-    [TestMethod]
-    public void SetQuantityOfBread_ReturnUpdatedQuantityOfBread_Int()
-    {
-      Bread newBread = new Bread("Baguette", 5, 1);
-      int updatedQuantity = 2;
-      newBread.Quantity = updatedQuantity;
-      int result = newBread.Quantity;
-      Assert.AreEqual(updatedQuantity, result);
-    }
-
-    [TestMethod]
-    public void GetDiscountOfBread_ReturnDiscountOfBread_Int()
-    {
-      Bread newBread = new Bread("Baguette", 15, 3);
-      int result = newBread.GetDiscount(3);
-      Assert.AreEqual( 10 ,result);
-    }
-  }
-
-  [TestClass]
+[TestClass]
   public class PastryTests
   {
     [TestMethod]
@@ -134,4 +66,3 @@ namespace Bakery.Tests
       Assert.AreEqual( 5 , newPastry.GetDiscount(3));
     }
   }
-}
